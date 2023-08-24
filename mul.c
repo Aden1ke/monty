c5f40306c53acbaf0f2dd90cef516d3cff5d4b46
Aden1ke <sarahogunlalu@gmail.com>
2023-08-24 08:01:19 -0700

    mul

---
+++ mul.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+/**
+ * mul - multiply the top two elements of the stack
+ * @stack: stack to manipulate
+ * @line_number: line number
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n += (*stack)->n;
+	pop(stack, line_number);
+}
--- push_pall.c
+++ push_pall.c
@@ -100,20 +100,3 @@ void sub(stack_t **stack, unsigned int line_number)
 	pop(stack, line_number);
 	(*stack)->n = result;
 }
-
-/**
- * mul - multiply the top two elements of the stack
- * @stack: stack to manipulate
- * @line_number: line number
- */
-void mul(stack_t **stack, unsigned int line_number)
-{
-	if (*stack == NULL || (*stack)->next == NULL)
-	{
-		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	(*stack)->next->n += (*stack)->n;
-	pop(stack, line_number);
-}
