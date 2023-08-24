a0b4666251246f74216f1652dc3923d61397d7b4
lazy_coder <ayomidesufian@gmail.com>
2023-08-23 18:25:50 -0700

    monty project

--- pint.c
+++ pint.c
@@ -25,3 +25,23 @@ void nop(void)
 {
 	/* nothing to see here*/
 }
+/**
+ * swap - swap two elements in a stack
+ * @stack: stack in reference
+ * @line_number: line number
+ * Return: void
+ */
+void swap(stack_t **stack, int line_number)
+{
+	int temp;
+	stack_t *node;
+	if (stack == NULL || (*stack) == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short.\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	node = (*stack)->next;
+	temp = node->n;
+	node->n = (*stack)->n;
+	(*stack)->n = temp;
+}
