d389fc7c3bc8d4684e3e9afdf2de4de4c89b56e8
yomitide <ayomidesufian@gmail.com>
2023-08-24 09:52:12 -0700

    advanced tasks

---
+++ div.c
@@ -0,0 +1,45 @@
+#include "monty.h"
+/**
+ * div - divide thr first two stack
+ * @stack: the stack in reference
+ * @line_number: line number
+ * Return: void
+ */
+void div(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0 || (*stack)->next->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	result = (*stack)->next->n / (*stack)->n;
+        pop(stack, line_number);
+        (*stack)->n = result;
+}
+/**
+ * mod - find the module of first two stack
+ * @stack: the stack in reference
+ * @line_number: line number
+ * Return: void
+ */
+void mod(stack_t **stack,unsigned int line_number)
+{
+        if (*stack == NULL || (*stack)->next == NULL)
+        {
+                fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+        if ((*stack)->n == 0 || (*stack)->next->n == 0)
+        {
+                fprintf(stderr, "L%d: division by zero\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+        result = (*stack)->next->n % (*stack)->n;
+        pop(stack, line_number);
+        (*stack)->n = result;
+}
