1a1b346609385e179264c681f61772b7b617db84
Aden1ke <sarahogunlalu@gmail.com>
2023-08-24 07:57:56 -0700

    test

--- push_pall.c
+++ push_pall.c
@@ -88,12 +88,32 @@ void add(stack_t **stack, unsigned int line_number)
  */
 void sub(stack_t **stack, unsigned int line_number)
 {
+	int result;
+
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	(*stack)->next->n -= (*stack)->n;
+	result = (*stack)->next->n - (*stack)->n;
+	pop(stack, line_number);
+	(*stack)->n = result;
+}
+
+/**
+ * mul - multiply the top two elements of the stack
+ * @stack: stack to manipulate
+ * @line_number: line number
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n += (*stack)->n;
 	pop(stack, line_number);
 }
