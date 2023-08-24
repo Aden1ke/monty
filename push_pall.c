33f80fc0fd1f48c9f02f67ade1558f027989d4c7
Aden1ke <sarahogunlalu@gmail.com>
2023-08-24 08:27:41 -0700

    logic test

--- push_pall.c
+++ push_pall.c
@@ -72,14 +72,18 @@ void pop(stack_t **stack, unsigned int line_number)
  */
 void add(stack_t **stack, unsigned int line_number)
 {
+	int result;
+
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	(*stack)->next->n += (*stack)->n;
+	result = (*stack)->next->n = (*stack)->n;
 	pop(stack, line_number);
+	(*stack)->n = result;
+
 }
 /**
  * sub - subtract the top two elements of the stack
@@ -96,7 +100,7 @@ void sub(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
-	result = (*stack)->next->n - (*stack)->n;
+	result = (*stack)->n - (*stack)->next->n;
 	pop(stack, line_number);
 	(*stack)->n = result;
 }
